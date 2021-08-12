da10acdeca694ebea42bc559b391902ca24f77ba
heiko <heikoatschroeder@outlook.de>
2021-08-13 01:18:30 +0200

    We changed hi to hello and added a new line

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f"Hello, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     print_hi("Heiko")
-    print("How are you?")
+    print("How are you doing?")
+    print("Where do you live?")
