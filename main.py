4edea33174ece1b4037eac19c7da297159c03a40
heiko <heikoatschroeder@outlook.de>
2021-08-13 01:25:21 +0200

    importing password

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from secret import password
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f"Hello, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
@@ -8,3 +11,4 @@ if __name__ == "__main__":
     print_hi("Heiko")
     print("How are you doing?")
     print("Where do you live?")
+    print(password)
